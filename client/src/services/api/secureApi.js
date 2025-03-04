1239b0eb833ff4097e915d1f3f381d978d1323ea
Julio Meza <juliomeza@gmail.com>
2025-03-04 14:57:24 -0500

    removi un comment

--- client/src/services/api/secureApi.js
+++ client/src/services/api/secureApi.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-//import { setupInterceptors } from './apiService';
 
 const apiProtected = axios.create({
   baseURL: 'http://localhost:8000/api/',
   withCredentials: true,
 });
-
-//setupInterceptors(apiProtected);
 
 export default apiProtected;
